d2085026b5650b7ba36f5967c1b3ed950634d782
vagrant <vagrant@homestead>
2017-05-11 15:43:51 +0000

    Transaction controller init

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Transaction $transaction)
+    {
+        //
+    }
+
+    public function transfer(Request $request)
+    {
+        echo 'TransactionController::transfer';
+        print_r($_POST);exit;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Transaction $transaction)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*
+    Version : 1.0
+    Url prefix: /api/1.0/xxxxx
+*/
 Route::group(['prefix' => '1.0'], function(){
+
     Route::resource('account', 'AccountController', ['only' => [
         'index', 'show', 'create', 'destroy'
     ]]);
+
+    Route::resource('transaction', 'TransactionController', ['only' => [
+        'index', 'show', 'create', 'transfer'
+    ]]);
+
+    Route::post('transaction/transfer', 'TransactionController@transfer');
+
+
 });
