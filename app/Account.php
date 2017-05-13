fcce6201e1977ebf3e8d4ae055d1936a724e11d8
vagrant <vagrant@homestead>
2017-05-13 17:11:06 +0000

    Models

--- app/Account.php
+++ app/Account.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+
+    /**
+     * Get transactions for this account
+     */
+    public function transactions(){
+        return $this->hasMany('App\Transaction');
+    }
 }
---
+++ config/bank.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'service_charge' => 100,
+    'check_ext_account_url' => 'http://handy.travel/test/success.json',
+];
